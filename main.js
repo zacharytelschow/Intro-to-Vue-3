8cc8d28e5a92ffa42c262dd78675582d8fbec1ce
Zachary Telschow <zacharytelschow@gmail.com>
2020-09-24 18:32:43 -0400

    End of Lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description: 'Made with 100% Merino wool, these socks are the best you can buy!'
+        };
+    }
+})
